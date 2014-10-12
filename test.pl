bbdd8f6ec1a3eb8aa11a021c332663820c295646
rootking <elliotps932@gmail.com>
2014-10-12 15:19:33 +0000

    made correct data in forms persistent!

--- test.pl
+++ test.pl
@@ -19,9 +19,8 @@ print '
 
 
 * {
-
 font-family: \'Vollkorn\', serif;
-margin:10px;
+margin-top:10px;
 padding:10px;
 background:rgba(0,0,0,0.1);
 }
@@ -32,7 +31,7 @@ our %forms = (
     first_name => {
         human_readable => "First Name",
         example        => "John",
-        regex          => '^[a-zA-Z\']{1,5}$',
+        regex          => '^[a-zA-Z\']{1,15}$',
         fix_data       => 'use letters from the english alphabet only.'
     },
 
@@ -44,54 +43,65 @@ our %forms = (
     },
 
     gender => {
-        regex    => '^[mf]{1,1}$',
-        fix_data => 'enter m or f.',
-	human_readable => "Gender",
+        regex          => '^[mf]{1,1}$',
+        fix_data       => 'enter m or f.',
+        human_readable => "Gender",
     },
 
     years_running => {
-        regex    => '^\d{1,2}$',
-        function => 'range(0,19)',
-        fix_data => 'enter 0 through 19.',
-	human_readable => "Years Running",
+        regex          => '^\d{1,2}$',
+        function       => 'range(0,19)',
+        fix_data       => 'enter 0 through 19.',
+        human_readable => "Years Running",
     },
 
-
     year_of_birth => {
-        regex    => '^[0-9]{4,4}$',
-        function => 'range(1900,2099)',
-        fix_data => 'enter 1900 through 2099.',
-	human_readable => "Year of Birth"    
-},
+        regex          => '^[0-9]{4,4}$',
+        function       => 'range(1900,2099)',
+        fix_data       => 'enter 1900 through 2099.',
+        human_readable => "Year of Birth"
+    },
 
-      currently_on_team => {
-        regex    => '^[yn]{1,1}$',
-        fix_data => 'enter either y or n.',
-	human_readable => "Currently on Team?",
-      },
+    currently_on_team => {
+        regex          => '^[yn]{1,1}$',
+        fix_data       => 'enter either y or n.',
+        human_readable => "Currently on Team?",
+    },
 
 );
 
-our $form_title = "Enter Runners...";
-our @array_of_form_hashes = ( 'first_name', 'last_name', 'year_of_birth', 'currently_on_team', 'years_running' );
-
-create_form( $form_title, @array_of_form_hashes );
+our $form_title           = "Enter Runners...";
+our @array_of_form_hashes = (
+    'first_name',        'last_name',
+    'gender',            'year_of_birth',
+    'currently_on_team', 'years_running'
+);
 
 sub create_form {
     my $form_title = shift;
     my (@form_fields) = @_;
     print "<div class=\"container\"><form>";
-    print "<form action=\"$0\" method=\"post\"><h1>$form_title</h1> <br>";
-    foreach my $field (@form_fields) {
+    print "<form><h1>$form_title</h1><br>";
+    unless(%errors) {undef %valid;}
+	foreach my $field (@form_fields) {
         if ( $forms{$field} ) {
-            print
-"$forms{$field}{human_readable}: <input type=\"text\" name=\"$field\" value=\"$forms{$field}{example}\"><br>";
+            if ( $errors{$field} ) {
+                print
+"<b>$forms{$field}{human_readable}:</b> <input type=\"text\" name=\"$field\" > <b>Please $errors{$field}</b> <br>";
+            }
+            else {
+                print
+"$forms{$field}{human_readable}: <input type=\"text\" name=\"$field\"";
+                if ( $valid{$field} ) { print "value=\"$valid{$field}\""; }
+                print "><br>";
+
+            }
         }
 
         #print "$field->{human_readable}... Example: $field->{example}<br>\n";
     }
-print "<input type=\"submit\" value=\"Submit\" >";    
-print "</form></div>";
+    print "<input type=\"submit\" value=\"Submit\" >";
+    print "</form></div>";
 }
 
 my $dbh = DBI->connect( 'dbi:mysql:team', 'team', 'teampasswd' );
@@ -106,6 +116,8 @@ for my $outside_ref (@$column_info_ref) {
 our %functions = (
     range => sub {
         my ( $low_end, $high_end, $data ) = @_;
+
+
         if ( ( $data > $low_end ) and ( $data < $high_end ) ) {
             return "True";
         }
@@ -113,10 +125,14 @@ our %functions = (
 );
 our @keys;
 our @values;
-our $invalid;
+our %valid;
+
 foreach $key ( keys %hash ) {
     our $value = $q->param($key);
     validate_form2( $key, $value );
+    unless ( $errors{$key} ) {
+        $valid{$key} = $value;
+    }
     if ( $value =~ m/(\D)/ ) { $value = "\"$value\""; }
     push @keys,   $key;
     push @values, $value;
@@ -124,6 +140,7 @@ foreach $key ( keys %hash ) {
 
 $key_scalar   = join( ',', @keys );
 $value_scalar = join( ',', @values );
+
 our %errors;
 
 sub validate_form2 {
@@ -141,8 +158,7 @@ sub validate_form2 {
 
             }
 
-            if (    $forms{$form_name}->{function}
-                and $functions{ $forms{$form_name}->{function} } )
+            if (    $forms{$form_name}->{function})
             {
                 my ( $function, $args ) =
                   $forms{$form_name}->{function} =~ m/^(.+)\((.+)\)$/;
@@ -162,22 +178,20 @@ sub validate_form2 {
 
 }
 
+create_form( $form_title, @array_of_form_hashes );
 ################################### TO DO: MAKE SURE THAT THE $KEYS ARE A SUBSET OF THE COLUMS OF A THE TABLE.. IF NOT, THEN SKIP THEM
 my $sqlz = "INSERT INTO runners ($key_scalar) VALUES ($value_scalar)";
 print $sqlz;
-=insert_into_array
+
 if ( !%errors and $key_scalar ) {
     my $insert_data = $dbh->prepare($sqlz);
     $insert_data->execute;
 }
-=cut
-
 
 print '
 <link href=\'http://fonts.googleapis.com/css?family=Vollkorn:400,400italic,700\' rel=\'stylesheet\' type=\'text/css\'>
 ';
 
-=show_tables
 my $sql = q/select * from runners/;
 my $sth = $dbh->prepare($sql);
 $sth->execute;
@@ -191,15 +205,5 @@ foreach my $row ( @{$rows} ) {
     $tr->multiply( 'td', \@{$row} );
 }
 print $table->text();
-=cut
-
-if (%errors) {
-    print '<div class="errors">';
-    foreach $key ( keys %errors ) {
-        print
-"<div class=\"error\"> <b>Invalid Entry</b>: For $key, Please $errors{$key}<br> </div>";
-    }
-    print '</div>';
-}
 
 print $cgi->end_html;
