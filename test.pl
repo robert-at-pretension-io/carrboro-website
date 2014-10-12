6c8ddd8a76d3e4e614df34d55199791a2d34c654
rootking <elliotps932@gmail.com>
2014-10-12 00:03:02 +0000

    made a more intuitive data validation structure to allow for future-proofing

--- test.pl
+++ test.pl
@@ -96,18 +96,46 @@ our %forms = (
         human_readable => "First Name",
         example        => "John",
         regex          => '^[a-zA-Z\']{1,5}$',
-    	fix_data => 
-	},
+        fix_data       => 'use letters from the english alphabet only.'
+    },
 
     last_name => {
         human_readable => "Last Name",
         example        => "Smith",
+        regex          => '^[a-zA-Z\'-]{1,15}$',
+        fix_data       => 'use letters from the english alphabet only.'
     },
 
-    age => {
-        human_readable => "Age of Runner",
-        example        => "19",
+    gender => {
+        regex    => '^[mf]{1,1}$',
+        fix_data => 'enter m or f.'
+
     },
+
+    years_running => {
+        regex    => '^\d{1,2}$',
+        function => 'range(0,19)',
+        fix_data => 'enter 0 through 19.',
+
+    },
+
+      graduation_year => {
+        regex    => '^[0-9]{4,4}$',
+        function => 'range(2000,2099)',
+        fix_data => 'enter 2000 through 2099.'
+
+      },
+
+    year_of_birth => {
+        regex    => '^[0-9]{4,4}$',
+        function => 'range(1900,2099)',
+        fix_data => 'enter 1900 through 2099.'
+    },
+
+      currently_on_team => {
+        regex    => '^[yn]{1,1}$',
+        fix_data => 'enter either y or n.',
+      },
 
 );
 
@@ -129,7 +157,8 @@ sub create_form {
 
         #print "$field->{human_readable}... Example: $field->{example}<br>\n";
     }
-    print "</form></div>";
+print "<input type=\"submit\" value=\"Submit\" >";    
+print "</form></div>";
 }
 
 my $dbh = DBI->connect( 'dbi:mysql:team', 'team', 'teampasswd' );
@@ -140,32 +169,6 @@ our @array_of_table_columns;
 for my $outside_ref (@$column_info_ref) {
     push( @array_of_table_columns, ${$outside_ref}[3] );
 }
-our @valid_form = (
-    [
-        "first_name",
-        'REGEX:^[a-zA-Z\' ]{1,15}$',
-        "use letters from the english alphabet only."
-    ],
-    [
-        "last_name",
-        'REGEX:^[a-zA-Z\'-]{1,15}$',
-        "use letters from the english alphabet only."
-    ],
-    [ "gender", 'REGEX:^[mf]{1,1}$', "enter m or f." ],
-    [
-        "years_running",        'REGEX:^\d{1,2}$',
-        'FUNCTION:range(0,19)', "enter 0 through 19."
-    ],
-    [
-        "graduation_year",           'REGEX:^[0-9]{4,4}$',
-        'FUNCTION:range(2000,2099)', "enter 2000 through 2099."
-    ],
-    [
-        "year_of_birth",             'REGEX:^[0-9]{4,4}$',
-        'FUNCTION:range(1900,2099)', "enter 1900 through 2099."
-    ],
-    [ "currently_on_team", 'REGEX:^[yn]{1,1}$', "enter either y or n." ]
-);
 
 our %functions = (
     range => sub {
@@ -180,7 +183,7 @@ our @values;
 our $invalid;
 foreach $key ( keys %hash ) {
     our $value = $q->param($key);
-    validate_form( $key, $value );
+    validate_form2( $key, $value );
     if ( $value =~ m/(\D)/ ) { $value = "\"$value\""; }
     push @keys,   $key;
     push @values, $value;
@@ -190,37 +193,53 @@ $key_scalar   = join( ',', @keys );
 $value_scalar = join( ',', @values );
 our %errors;
 
-sub validate_form {
-    our ( $form_name, $form_data, ) = @_;
-    foreach $form (@valid_form) {
-        if ( $form_name =~ m/@{$form}[0]/ ) {
-            foreach $criteria ( @{$form} ) {
-                if ( $criteria =~ m/REGEX:(.+)$/ ) {
-                    unless ( $form_data =~ m/$1/ ) {
-                        unless ( $errors{$form_name} ) {
-                            $errors{$form_name} = @{$form}[-1];
-                        }
+sub validate_form2 {
+
+    our ( $form_name, $form_data ) = @_;
+    foreach $form ( keys %forms ) {
+
+        if ( $forms{$form_name} ) {
+            if ( $forms{$form_name}->{regex} ) {
+                unless ( $form_data =~ m/$forms{$form_name}{regex}/ ) {
+                    unless ( $errors{$form_name} ) {
+                        $errors{$form_name} = $forms{$form_name}{fix_data};
                     }
                 }
-                if ( $criteria =~ m/FUNCTION:(.+)\((.+)\)$/ ) {
-                    @array = split( /,/, $2 );
-                    push( @array, $form_data );
-                    unless ( $functions{$1}->(@array) ) {
-                        unless ( $errors{$form_name} ) {
-                            $errors{$form_name} = @{$form}[-1];
-                        }
+
+            }
+
+            if (    $forms{$form_name}->{function}
+                and $functions{ $forms{$form_name}->{function} } )
+            {
+                my ( $function, $args ) =
+                  $forms{$form_name}->{function} =~ m/^(.+)\((.+)\)$/;
+                @array = split( /,/, $args );
+                push( @array, $form_data );
+                unless ( $functions{$function}->(@array) ) {
+
+                    unless ( $errors{$form_name} ) {
+                        $errors{$form_name} = $forms{$form_name}{fix_data};
                     }
+
                 }
             }
         }
+
     }
+
 }
+
 ################################### TO DO: MAKE SURE THAT THE $KEYS ARE A SUBSET OF THE COLUMS OF A THE TABLE.. IF NOT, THEN SKIP THEM
 my $sqlz = "INSERT INTO runners ($key_scalar) VALUES ($value_scalar)";
+print $sqlz;
+=insert_into_array
 if ( !%errors and $key_scalar ) {
     my $insert_data = $dbh->prepare($sqlz);
     $insert_data->execute;
 }
+=cut
+
+
 print '
 <link href="http://fonts.googleapis.com/css?family=Corben:bold" rel="stylesheet" type="text/css">
 <link href="http://fonts.googleapis.com/css?family=Nobile" rel="stylesheet" type="text/css">
@@ -242,7 +261,6 @@ foreach my $row ( @{$rows} ) {
 print $table->text();
 =cut
 
-=show_errors
 if (%errors) {
     print '<div class="errors">';
     foreach $key ( keys %errors ) {
@@ -251,6 +269,5 @@ if (%errors) {
     }
     print '</div>';
 }
-=cut
 
 print $cgi->end_html;
